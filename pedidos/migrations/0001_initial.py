00182acbe0211ea636d056d16bb8ac3927212367
Dario Ramon Gonzalez <93885597+DARIORAMON@users.noreply.github.com>
2026-01-12 20:48:35 -0300

    Update 0001_initial.py se borra  00002

--- pedidos/migrations/0001_initial.py
+++ pedidos/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('productos', '0002_producto_es_oferta_producto_precio_oferta'),
+        
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
