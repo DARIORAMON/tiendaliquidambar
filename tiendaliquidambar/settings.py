5a6c21be4c19b54720503e0f884679ebf383fdf6
dariorg <dario.gonzalez@educ.ar>
2026-01-12 11:29:57 -0300

    Corregida ruta de static y activado Cloudinary correctamente

--- tiendaliquidambar/settings.py
+++ tiendaliquidambar/settings.py
@@ -158,7 +158,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Asegúrate de que este nombre coincida EXACTAMENTE con tu carpeta en VS Code
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_dev"), 
+    os.path.join(BASE_DIR, "static"), 
 ]
 
 # 3. Almacenamiento optimizado
